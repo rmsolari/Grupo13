74f807d9ab02d40a7172edeae3d389b691ea1339
icontrerasn <icontrerasnarbona@gmail.com>
2017-04-29 20:45:32 -0300

    Help

--- tvseries4u/app/models/serie.rb
+++ tvseries4u/app/models/serie.rb
@@ -2,5 +2,4 @@ class Serie < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
-  validates :nombre, presence: true, length: { maximum:60 }
 end
