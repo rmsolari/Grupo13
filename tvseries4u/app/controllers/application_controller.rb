facd9221be6d6b58668dd4ef67b36f353536a32a
icontrerasn <icontrerasnarbona@gmail.com>
2017-04-29 03:02:30 -0300

    db fix

--- tvseries4u/app/controllers/application_controller.rb
+++ tvseries4u/app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "Bienvenido a TVSeries4u!!"
+    render html: "Bienvenido a TVSeries4u"
   end
 end
