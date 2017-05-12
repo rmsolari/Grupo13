394053c910c9d86d60f93e404a80769cced5313c
rmsolari <rmsolari@uc.cl>
2017-05-12 15:53:18 -0300

    interfaz con mejoras

--- tvseries4u/app/controllers/application_controller.rb
+++ tvseries4u/app/controllers/application_controller.rb
@@ -5,4 +5,5 @@ class ApplicationController < ActionController::Base
   def hello
     render html: "Bienvenido a TVSeries4u"
   end
+
 end
--- tvseries4u/config/routes.rb
+++ tvseries4u/config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
@@ -10,6 +8,8 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+  get '/new_movie', to: 'movies#create'
   resources :users do
     member do
       get :following, :followers
