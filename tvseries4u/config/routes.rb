d5d726e44da478420a9c95dd129a2529abc86c5f
icontrerasn <icontrerasnarbona@gmail.com>
2017-05-19 16:33:38 -0400

    delete for series

--- tvseries4u/config/routes.rb
+++ tvseries4u/config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   post '/signup',  to: 'users#create'
   get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
+  post '/login', to: 'sessions#create'  
   delete '/logout', to: 'sessions#destroy'
   match 'users/:id' => 'users#destroy', :via => :delete, :as => :admin_destroy_user
 
