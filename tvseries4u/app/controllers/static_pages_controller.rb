c16501e91b50c0b7072494c0d245ee7a50ca4f3f
rmsolari <rmsolari@uc.cl>
2017-06-25 06:13:11 -0400

    mejoras de interfaz

--- tvseries4u/app/controllers/static_pages_controller.rb
+++ tvseries4u/app/controllers/static_pages_controller.rb
@@ -3,6 +3,7 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @movie  = current_user.movies.build
+      @user=current_user
       @feed_items = Movie.accessible_by(current_ability)
     end
   end
